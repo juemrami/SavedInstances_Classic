ef09ab37d669efbf9e759dd692c71528ba2095c5
Julio Ramirez <juemrami@gmail.com>
2025-08-12 10:03:21 -0700

    [hotfix][mop]: add August Stone Fragment to currencies
    - newly added currencies may not be enabled by default, be sure to check in addon's currency settings.

--- SavedInstances/Modules/Currency.lua
+++ SavedInstances/Modules/Currency.lua
@@ -217,6 +217,7 @@ local modernClassicCurrencies = {
   },
   -- Mists of Pandaria
   [EXPANSION_NAME4] = Expansion.Current >= Expansion.Mists and {
+    3350, -- August Stone Fragment
     402, -- Ironpaw Token
     697, -- Elder Charm of Good Fortune
     698, -- Zen Jewelcrafter's Token
